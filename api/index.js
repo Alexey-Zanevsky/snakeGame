a05c6ad482edf66ba4b8e38c8088327e325bf56c
Aliaksei Zaneuski <aliakseizaneuski@MacBook-Air-Aliaksei.local>
2026-06-08 02:30:09 +0200

    Improve path handling in Express

--- api/index.js
+++ api/index.js
@@ -7,8 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Get the root directory (parent of api/)
+const rootDir = path.resolve(__dirname, '..');
+
 // Serve static files from project root
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(rootDir));
 
 // Public demo version - no authentication or database
 app.use('/auth', (req, res) => {
@@ -17,7 +20,7 @@ app.use('/auth', (req, res) => {
 
 // Fallback to index.html for SPA
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
+    res.sendFile(path.join(rootDir, 'index.html'));
 });
 
 module.exports = app;
